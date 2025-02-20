161d159c3676264862f5976ce933f15262ad44d6
Naresh Sondkar <nareshsondkar30218@gmail.com>
2025-02-20 11:18:33 +0530

    add a salary repo method

--- src/main/java/com/employeeapi/repositories/SalaryRepopsitory.java
+++ src/main/java/com/employeeapi/repositories/SalaryRepopsitory.java
@@ -1,6 +1,7 @@
 package com.employeeapi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.employeeapi.entities.Employee;
@@ -9,4 +10,7 @@ import com.employeeapi.entities.EmployeeSalary;
 @Repository
 public interface SalaryRepopsitory extends JpaRepository<EmployeeSalary, Integer>{
 	public EmployeeSalary findByEmployee(Employee employee);
+	
+	@Query("SELECT MAX(es.salary) FROM EmployeeSalary es")
+	public double findMaxSalary();
 }
