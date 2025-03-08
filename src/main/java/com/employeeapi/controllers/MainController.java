9e3ae7fd7efe86a7c019cedab91dfd0312b6c082
Naresh30218 <84191209+Naresh30218@users.noreply.github.com>
2025-03-08 21:44:03 +0530

    Create MainController.java

---
+++ src/main/java/com/employeeapi/controllers/MainController.java
@@ -0,0 +1,22 @@
+package com.employeeapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    // Welcome message for the root URL
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Employee Management System!";
+    }
+
+    // Optional: Redirect from root URL to /employees
+    /*
+    @GetMapping("/")
+    public String redirectToEmployees() {
+        return "redirect:/employees";
+    }
+    */
+}
